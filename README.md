feaae906abdca4e8ef3af79886efc11763f7066c
ffturan <fehmifturan@gmail.com>
2020-10-15 15:54:59 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# AWS Lambda func. for monitoring EC2 Instance State Changes
+# AWS Lambda for monitoring EC2 Instance State Changes
+
+It is possible to configure a CloudWatch rule for EC2 instance state changes and directly associate with AWS SNS however if you choose that path, SNS message will show EC2 instance ID and it's almost impossible to know which server is actually started, stopped, terminated through that ID number.  
+
+That's why I prefer getting that SNS nitification through AWS Lambda function, so we can get the actual server name.  
+## Requirements
+Lambda function will need IAM role with
+	- arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+	- arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess
+	- Custom SNS publish policy for target SNS topic 
+## Output
+```shell
+Sample SNS message:
+Instance: development-test-01 / i-00dc7daa6a199375f inside account: ABC now in new state: RUNNING Time: 2020-10-15 12:52:57.084672
+```
---
+++ ec2-state-watchdog.py
@@ -0,0 +1,49 @@
+# Get EC2 Instance state changes with date and server name
+# Sends SNS notification
+# Fehmi-10/20
+
+import json
+import boto3
+import time
+import os
+
+#
+vAccountName="ABC"
+#
+
+def lambda_handler(event, context):
+    print(context)
+    # Create an SNS client
+    worker_sns = boto3.client('sns')
+    
+    # Create an EC2 client
+    worker_ec2 = boto3.client('ec2')
+    
+    #Fix local time
+    os.environ['TZ'] = 'US/Eastern'
+    time.tzset()
+    # Info that we need to know
+    vNow=time.strftime('%X %x %z')
+    vInstanceId=event['detail']['instance-id']
+    vInstanceState=event['detail']['state']
+    
+    # Try to find EC2 instance name
+    response = worker_ec2.describe_instances( InstanceIds=[vInstanceId,],)
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            try:
+                for item in instance["Tags"]:
+                    if item['Key'] == 'Name':
+                        vInstanceName=item['Value']
+            except KeyError as e:
+                    print(f' Instance {vInstanceId} has NO tags ')
+                    vInstanceName="Null"
+    
+    vMessage="Instance: " +  vInstanceName + " / " + vInstanceId + " inside account: " + vAccountName +  " now in new state: " + vInstanceState.upper() + " Time: " + str(vNow)
+    
+    # Publish a simple message to the specified SNS topic
+    response = worker_sns.publish(
+        TopicArn='arn:aws:sns:us-east-1:111111111111:ops-alert',
+        Subject='EC2 State Change',
+        Message=vMessage,    
+    )
